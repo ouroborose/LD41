db485a2d78ec21b9474c5f7399feb719ca2e82a1
stregawolf <stregawolf@gmail.com>
2018-04-22 01:38:12 -0700

    Integrated player2 and added textmesh pro

--- Assets/LD41/Scripts/Main.cs
+++ Assets/LD41/Scripts/Main.cs
@@ -26,12 +26,10 @@ public class Main : Singleton<Main> {
         CameraController.Instance.transform.position += new Vector3(LevelGenerator.Instance.m_totalWidth / 2.0f, 0.0f, LevelGenerator.Instance.m_totalHeight / 2.0f);
 
         BaseActor player1 = CreateActor(m_player1Prefab, LevelGenerator.Instance.GetPlayer1SpawnTile().transform.position + Vector3.up * m_playerSpawnOffset);
-        player1.SetColor(Color.blue);
         player1.m_playerId = PlayerData.PlayerId.PLAYER_1;
         m_players[0].m_character = player1;
 
         BaseActor player2 = CreateActor(m_player2Prefab, LevelGenerator.Instance.GetPlayer2SpawnTile().transform.position + Vector3.up * m_playerSpawnOffset);
-        player2.SetColor(Color.red);
         player2.m_playerId = PlayerData.PlayerId.PLAYER_2;
         m_players[1].m_character = player2;
     }
