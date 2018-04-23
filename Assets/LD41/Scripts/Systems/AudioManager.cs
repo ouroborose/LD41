9a4cfe5d66773ee8ad55f2a698deb7838d6a3277
johan <kuyajohan@gmail.com>
2018-04-22 23:32:49 -0400

    wip audio

--- Assets/LD41/Scripts/Systems/AudioManager.cs
+++ Assets/LD41/Scripts/Systems/AudioManager.cs
@@ -5,10 +5,28 @@ using UnityEngine;
 public class AudioManager : Singleton<AudioManager> {
     [SerializeField] protected AudioSource m_bgMusic;
     [SerializeField] protected AudioSource m_panicMusic;
+    [SerializeField] protected AudioSource m_titleMusic;
+
+    
+    public void StartStageMusic()
+    {
+        m_bgMusic.Play();
+        m_titleMusic.Stop();
+        m_panicMusic.Stop();
+    }
 
     public void StartPanicMusic()
     {
-        m_panicMusic.Play();
+        
         m_bgMusic.Stop();
+        m_titleMusic.Stop();
+        m_panicMusic.Play();
+    }
+
+    public void StartTitleMusic()
+    {
+        m_bgMusic.Stop();
+        m_titleMusic.Play();
+        m_panicMusic.Stop();
     }
 }
